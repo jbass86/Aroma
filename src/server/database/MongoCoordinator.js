e4cd108711f3526b9419567e007e3c9e16f61b44
Josh Bass <jbass86@gmail.com>
2016-06-15 21:55:54 -0400

    talked to kate and chagned the colors alittle bit

--- src/server/database/MongoCoordinator.js
+++ src/server/database/MongoCoordinator.js
@@ -9,15 +9,23 @@ module.exports = class MongoCoordinator {
 
     var _this = this;
 
-    var MongoClient = mongodb.MongoClient;
-    MongoClient.connect("mongodb://localhost:27017/aroma", (err, db) => {
-
-      if (err){
-        //gonna need some retry logic here...
-        console.log("something bad happened!!!");
-      }
-      _this.db = db;
-    })
+    var connectTask = function(){
+      var MongoClient = mongodb.MongoClient;
+      MongoClient.connect("mongodb://localhost:27017/aroma", (err, db) => {
+        if (err){
+          //gonna need some retry logic here...
+          console.error("Something bad happened!!!");
+          if (db){
+            db.close();
+          }
+          setTimeout(connectTask, 5000);
+        }else{
+          console.log("Mongo Success!!!");
+          _this.db = db;
+        }
+      });
+    }
+    connectTask();
   }
 
   getDatabaseInstance() {
